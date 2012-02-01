27c4fa93f9db5b12595f3ce1021a3595af3a8872
Assen <assen@belcho.(none)>
2012-02-01 19:00:41 +0100

    To leiningen

--- project.clj
+++ project.clj
@@ -5,7 +5,10 @@
                  [compojure "0.6.2"]
                  [ring/ring-core "0.3.7"]
                  [ring/ring-jetty-adapter "0.3.7"]
-                 [clj-stacktrace "0.2.4"]]
+                 [clj-stacktrace "0.2.4"]
+                 [clj-http "0.3.0"]
+                 [org.danlarkin/clojure-json "1.2-SNAPSHOT"]
+                 [enlive "1.0.0" ]]
   :dev-dependencies [[ring/ring-devel "0.3.7"]
                      [lein-ring "0.4.0"]]
   :ring {:handler hello-clojure-web.core/app})
--- src/test/clojure/brosor/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns brosor.test.core
-  (:use [brosor.core])
-  (:use [clojure.test]))
-
-(deftest replace-me
-  (is false "No tests have been written."))
